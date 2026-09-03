3fe49be0765e5a446ac25c58305fde9da9d5f8f7
Martin Jõgeva <martinj6geva@gmail.com>
2026-09-03 09:38:46 +0300

    Add types example

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo 'Hello world!';
-?>
+$test = 10;
+$test = 3.1;
+$test = 'Hello World!';
+$test = 'õpilane';
+$test = true;
+$test = false;
+$test = null;
+$test = [1, null, true, 'lol', 2, 3.2];
+$test = new stdClass();
+$test->name = 'Martin' ;
+var_dump($test);
