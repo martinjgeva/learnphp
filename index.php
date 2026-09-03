89f914fe52e8b5d6c0ae56e50d876f9883da0566
Martin Jõgeva <martinj6geva@gmail.com>
2026-09-03 09:50:34 +0300

    Add math examples

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = 'Hello World!';
-$test = 'õpilane';
-$test = true;
-$test = false;
-$test = null;
-$test = [1, null, true, 'lol', 2, 3.2];
-$test = new stdClass();
-$test->name = 'Martin' ;
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = 10 / 10;
+$test = $test + 10;
+$test = 2 ** 3; 
+$test = 5 % 2;
+$test += 3;
+$test -= 3;
+$test *= 3;
+$test /= 3;
+$test %= 3;
+$test **= 3;
+$test %= 3;
+$test++;
+$test--;
+--$test;
+++$test;
+
+
+$a = 1;
+$b = ++$a;
+var_dump($b, $a);
+
+$test = sin(pi());
 var_dump($test);
