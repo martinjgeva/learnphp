2d46e1b0460ad58eb17c0f084308925800dd6cec
Martin Jõgeva <martinj6geva@gmail.com>
2026-09-08 08:43:09 +0300

    Add multiline string example

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ $test = "$name is $age years old!";
 $test = '$name is $age years old!';
 $test = 'don\'t';
 $test = "lol\nlol\n";
-
-
+$test = <<<END
+    $age $name
+      a
+     b
+    c
+\n
+END;
 var_dump($test);
