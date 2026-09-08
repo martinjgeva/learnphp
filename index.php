6d70671f79c8657b356bd92ae7b70cf0415583f0
Martin Jõgeva <martinj6geva@gmail.com>
2026-09-08 09:58:25 +0300

    Add loops example

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <?php
-$test = array(1, 2, 3);
-$test = [1, 2, 3];
-$test = [1, 'tauri', true, [1, 2, 3]];
-$test = [
-    'name' => 'Pauri',
-    'age' => '4',
-    3,
-    'tuhala',
-    true,
-    100 => 'lol',
-    'troll',
-];
-var_dump($test['name']);
-array_push($test, 'added', 'tuhalakiller', 3);
-$test[] = 'new wallahi';
-$test['name'] = 'Martin';
-$test[1] = 9999999;
-unset($test[3]);
-var_dump($test);
+
+for($i = 0; $i<10; $i++){
+    var_dump($i);
+}
+
+for($i = 9; $i<10; $i--){
+    var_dump($i);
+}
+
+for($i = 2; $i<1_000_000; $i*=2){
+    var_dump($i);
+}
+$time = time();
+while(time() < $time+1){
+    var_dump($i++);
+}
+
+while(false) {
+    var_dump('WHILE');
+}
+do {
+    var_dump('DO');
+} while(false);
+
+// while(true){
+
+//}
+
+$fruits = ['Apple', 'Banana', 'Cherry'];
+foreach($fruits as $fruit){
+    var_dump($fruit);
+}
+
+foreach($fruits as $key=>$fruit){
+    var_dump($key,$fruit);
+    $fruit = 'asdas';
+}
+var_dump($fruits);
