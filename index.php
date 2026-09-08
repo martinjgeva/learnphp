fbe489a297edcc08a7bec0498f9ff4190f329805
Martin Jõgeva <martinj6geva@gmail.com>
2026-09-08 09:23:32 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = 0 == false;
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+$test = [1, 'tauri', true, [1, 2, 3]];
+$test = [
+    'name' => 'Pauri',
+    'age' => '4',
+    3,
+    'tuhala',
+    true,
+    100 => 'lol',
+    'troll',
+];
+var_dump($test['name']);
+array_push($test, 'added', 'tuhalakiller', 3);
+$test[] = 'new wallahi';
+$test['name'] = 'Martin';
+$test[1] = 9999999;
+unset($test[3]);
 var_dump($test);
